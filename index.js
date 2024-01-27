aafea8eac13c99fbe5e55d327b803bcf37e2978d
Roland Levy <contact@rolandlevy.co.uk>
2024-01-27 23:55:00 +0000

    Added login route

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {connect, Schema, model } = require('mongoose');
+const { connect, Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 connect(MONGODB_URI, {});
@@ -22,6 +22,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/register.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
 app.post('/register', async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -34,6 +42,25 @@ app.post('/register', async (req, res) => {
     res.send(`New user registered: ${username}`);
   } catch (err) {
     res.send(err);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user) {
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (validPassword) {
+        res.send('Logged in');
+      } else {
+        res.send('Incorrect password');
+      }
+    } else {
+      res.send('Incorrect username');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
