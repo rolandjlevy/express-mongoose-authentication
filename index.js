1562691553785db5572bde779362edff286df516
Roland Levy <contact@rolandlevy.co.uk>
2024-01-27 20:25:01 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
