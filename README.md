26d9b88145115626d5261ae3d6fed1f7a35c7e39
Roland Levy <contact@rolandlevy.co.uk>
2024-01-27 22:58:02 +0000

    added register route

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Express.js Mongoose Authentication
 
+### Link
+
+- Manually generate bcrypt hash: [bcrypt-generator.com](https://bcrypt-generator.com)
+
 ### View the project in VS Code Simple browser
+
 - Get the URL for the page - see PORTS in terminal
 - From command palette: Simple Browser: Show
 - then paste in that URL
--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const {connect, Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+connect(MONGODB_URI, {});
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+const User = model('User', UserSchema);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      username,
+      password: hashedPassword
+    });
+    await user.save();
+    res.send(`New user registered: ${username}`);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port:${port}`);
